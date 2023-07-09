063770672741fc495aa1209757c76f04283271c4
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 09:09:29 -0500

    feat(project): tailwind color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 export default {
   content: [
     "./index.html",
     "./src/**/*.vue"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.white,
+        secondary: colors.emerald,
+        terciary: colors.black,
+        accent: colors.pink,
+      }
+    },
   },
   plugins: [],
 }
