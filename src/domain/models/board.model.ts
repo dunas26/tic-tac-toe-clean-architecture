f3b1f256e8f7d88182e115f60ba398830f76dd7d
dunas26 <jhonjamesillo@gmail.com>
2023-07-08 19:51:10 -0500

    feat: basic domain layout

---
+++ src/domain/models/board.model.ts
@@ -0,0 +1,31 @@
+import { TileValue } from "../values";
+import { Mark } from "../values/tile.value";
+
+export class BoardModel {
+    constructor(id: string, moveCount: number, tiles?: TileValue[]) {
+        this.id = id;
+        this.moveCount = moveCount;
+        this.setUpTiles(tiles);
+    }
+    public readonly id: string;
+    public readonly moveCount: number;
+    private tiles!: TileValue[];
+
+    public mark(tile: number, mark: Mark): this {
+        if (!this.validTileNumber(tile)) throw new Error("Invalid tile number");
+        this.tiles[tile] = new TileValue(mark);
+        return this;
+    }
+
+    private validTileNumber(tile: number): boolean {
+        if (this.tiles.length == 0) return false;
+        return tile >= 0 && tile < this.tiles.length;
+    }
+
+    private setUpTiles(tiles?: TileValue[]): this {
+        this.tiles = tiles
+            ? tiles
+            : Array<TileValue>(9).fill(new TileValue(""));
+        return this;
+    }
+}
