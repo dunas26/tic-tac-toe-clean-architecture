551d729836c4573f5e2536376f5a3a31b987d74a
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 09:09:43 -0500

    feat(main): page configuration

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from './presentation/ui/Button.vue';
+
+function onButtonClick(buttonName: string) {
+  console.log(buttonName, "was clicked");
+}
+
+</script>
 <template>
-  <h1 class="font-bold text-6xl">Tic tac toe</h1>
+  <main class="max-w-md md:my-2 md:border-2 border-terciary/10 rounded-lg flex flex-col mx-auto justify-start items-center bg-primary h-full">
+    <h1 class="font-bold text-6xl mt-[10rem] text-center text-secondary-600">Tic tac toe</h1>
+    <section class="flex flex-col flex-1 w-full gap-4 px-12 mt-[20rem]">
+      <Button @buttonClick="onButtonClick('Start game')" label="start game"/>
+      <Button @buttonClick="onButtonClick('Continue')" label="Continue" buttonStyle="secondary"/>
+    </section>
+    <footer class="h-auto p-8">
+      <p>Copyright &copy; John James Valencia</p>
+    </footer>
+  </main>
 </template>
