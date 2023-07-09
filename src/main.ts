8715a1be99a1b7e138fa8cdce6025683da80efc3
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 16:23:12 -0500

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import "reflect-metadata";
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import './style.css'
 import App from './App.vue'
 
 import { buildDependencies } from './config/project.dependencies'
 
-const app = createApp(App)
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(pinia);
+
 buildDependencies();
 app.mount('#app')
