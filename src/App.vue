cbf947c8f01c291dd0e5efa49d0ad00b363b26db
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 16:33:51 -0500

    feat: setup main game screen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
-import Button from './presentation/ui/Button.vue';
+import { ref } from "vue";
+import { dependency } from "./config/project.dependencies";
+import { AppController } from "./infrastructure/controllers";
+import Button from "./presentation/ui/Button.vue";
 
-function onButtonClick(buttonName: string) {
-  console.log(buttonName, "was clicked");
+const controller = dependency(AppController);
+const boardId = ref<string | undefined>(undefined);
+
+function startGame() {
+  boardId.value = controller?.startNewGame().id;
 }
 
+function continueGame() {
+  throw new Error("Not implemented");
+}
 </script>
 <template>
-  <main class="max-w-md md:my-2 md:border-2 border-terciary/10 rounded-lg flex flex-col mx-auto justify-start items-center bg-primary h-full">
-    <h1 class="font-bold text-6xl mt-[10rem] text-center text-secondary-600">Tic tac toe</h1>
+  <main
+    class="max-w-md md:my-2 md:border-2 border-terciary/10 rounded-lg flex flex-col mx-auto justify-start items-center bg-primary h-full"
+  >
+    <h1 class="font-bold text-6xl mt-[10rem] text-center text-secondary-600">
+      Tic tac toe
+    </h1>
+    <span class="text-terciary/40 mt-4" v-if="boardId">
+      Playing board: #{{ boardId }}
+    </span>
     <section class="flex flex-col flex-1 w-full gap-4 px-12 mt-[20rem]">
-      <Button @buttonClick="onButtonClick('Start game')" label="start game"/>
-      <Button @buttonClick="onButtonClick('Continue')" label="Continue" buttonStyle="secondary"/>
+      <Button @buttonClick="startGame()" label="start game" />
+      <Button
+        @buttonClick="continueGame()"
+        label="continue"
+        buttonStyle="secondary"
+      />
     </section>
     <footer class="h-auto p-8">
       <p>Copyright &copy; John James Valencia</p>
