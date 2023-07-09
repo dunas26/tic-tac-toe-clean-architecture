6881c32dd5785fde4f3cb1cb9613ce5ebfa03df4
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 08:29:44 -0500

    feat(button): component creation

---
+++ src/presentation/ui/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed, toRefs } from "vue";
+export interface ButtonProps {
+  label: string;
+  buttonStyle: "main" | "secondary";
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+    buttonStyle: "main"
+});
+const { label, buttonStyle } = toRefs(props);
+
+defineEmits(['buttonClick']);
+
+const colorStyles = computed(() => {
+  return buttonStyle.value == "main"
+    ? "bg-emerald-500 text-white focus:outline-white border-transparent"
+    : "bg-white text-emerald-500 focus:outline-emerald-500 border-emerald-500 focus:border-transparent";
+});
+</script>
+<template>
+  <button
+    :class="colorStyles"
+    class="relative rounded-md px-3 py-2 border shadow-sm overflow-hidden transition-[shadow,_transform,_color,_outline-color] hover:shadow-md hover:translate-y-[-2px] active:translate-y-[2px] outline outline-2 -outline-offset-4 outline-transparent active:shadow-none before:transition-[background-color] before:contents before:inset-0 before:absolute before:bg-transparent active:before:bg-black/20"
+    @click="$emit('buttonClick')"
+  >
+    {{ label }}
+  </button>
+</template>
