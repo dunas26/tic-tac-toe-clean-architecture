4da911eea5b4d1d02302b4cba94b29f10822b45b
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 11:15:09 -0500

    feat(project-dependencies): inject specification

--- src/config/project.dependencies.ts
+++ src/config/project.dependencies.ts
@@ -4,10 +4,10 @@ import { AppController } from "../controllers";
 type Constructor = new(...args: any[]) => any;
 
 export const buildDependencies = (app: Vue.App) => {
-    app.provide(typeof AppController, new AppController());
+    app.provide(AppController.name, new AppController());
 }
 
-export const inject = <T extends Constructor>(
+export const vueDependency = <T extends Constructor>(
     object: T
 ): InstanceType<T> | undefined => {
     return Vue.inject<T>(object.name) as InstanceType<T>;
