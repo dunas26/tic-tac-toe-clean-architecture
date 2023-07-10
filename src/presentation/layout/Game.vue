3f06b517c4c3ddb10e932f6ec061cec91837aeb1
dunas26 <jhonjamesillo@gmail.com>
2023-07-09 20:47:11 -0500

    feat: Game component board guard

--- src/presentation/layout/Game.vue
+++ src/presentation/layout/Game.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { dependency } from '../../config/project.dependencies';
+import { GameController } from '../../infrastructure/controllers';
 import Button from '../ui/Button.vue';
+import { BoardModel } from '../../domain/models';
+import { useRouter } from 'vue-router';
+
+const controller = dependency<GameController>('GamePort');
+const board = ref<BoardModel | undefined>(undefined);
+const router = useRouter();
+
+onMounted(() => {
+    board.value = controller?.getCurrentBoard();
+    if(!board.value) router.push({ name: 'home' });
+})
 
 </script>
 <template>
