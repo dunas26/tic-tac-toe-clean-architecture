7c7de7c8223ea2c1acbbf18cf40ef4241a12757e
dunas26 <jhonjamesillo@gmail.com>
2023-07-10 06:24:26 -0500

    feat: display current turn

---
+++ src/presentation/CurrentTurn.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed, toRefs } from 'vue';
+import { Mark } from '../domain/values/tile.value';
+
+export interface CurrentTurnProps {
+    mark: Mark;
+}
+const props = defineProps<CurrentTurnProps>();
+const { mark } = toRefs(props);
+
+const markColor = computed(() => {
+    return mark.value == "X" 
+    ? "text-red-400"
+    : "text-blue-400"
+})
+</script>
+<template>
+  <section
+    class="flex items-center justify-between p-6 bg-white shadow-md rounded-md border border-terciary/5"
+  >
+    <span class="text-xl font-semibold text-terciary/30">Current turn: </span>
+    <span 
+    class="font-extrabold text-2xl"
+    :class="markColor">{{ mark }}</span>
+  </section>
+</template>
--- src/presentation/layout/Game.vue
+++ src/presentation/layout/Game.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import Button from '../ui/Button.vue';
 import Board from '../Board.vue';
 import { dependency } from '../../config/project.dependencies';
 import { GameController } from '../../infrastructure/controllers';
 import { BoardModel } from '../../domain/models';
+import CurrentTurn from '../CurrentTurn.vue';
 
 const controller = dependency<GameController>('GamePort');
 const board = ref<BoardModel | undefined>(undefined);
@@ -16,6 +17,15 @@ onMounted(() => {
     if(!board.value) router.push({ name: 'home' });
     controller?.matchStart();
 })
+
+const nextMark = computed(() => {
+    return controller?.getCurrentMark();
+});
+
+const hasWinner = computed(() => {
+    if(!board.value) return false;
+    return controller?.getWinner(board.value as BoardModel);
+});
 
 function onTileClick(index: number) {
     const mark = controller?.getCurrentMark();
@@ -41,6 +51,9 @@ function quitGame() {
 </script>
 <template>
     <main class="flex flex-col w-full h-full">
+        <header class="p-12">
+            <CurrentTurn v-if="!hasWinner && nextMark" :mark="nextMark" />
+        </header>
         <section class="flex-1">
             <Board v-if="board" :board="(board as BoardModel)" @tile-click="onTileClick($event)"/>
         </section>
