cf0e5c35d8aa4020a9b7dfcc5f699b35c431cce6
dunas26 <jhonjamesillo@gmail.com>
2023-07-10 07:53:21 -0500

    feat: new timer features

--- src/infrastructure/composables/timer.ts
+++ src/infrastructure/composables/timer.ts
@@ -11,8 +11,12 @@ export function useTimer(updateMillis: number = 0) {
     const startTime = ref(0);
     const endTime = ref(0);
     const elapsed = ref(0);
+    const hasStarted = ref(false);
+    const finished = ref(false);
 
     const start = () => {
+        if(finished.value) return;
+        hasStarted.value = true;
         startTime.value = Date.now();
         interval = setInterval(() => {
             elapsed.value = Date.now() - startTime.value;
@@ -20,23 +24,32 @@ export function useTimer(updateMillis: number = 0) {
     };
 
     const stop = () => {
-        endTime.value = Date.now()
-        elapsed.value = endTime.value - startTime.value;
-        clearInterval(interval);
+        if(finished.value) return;
+        if (!hasStarted.value) {
+            startTime.value = Date.now();
+            endTime.value = startTime.value;
+            elapsed.value = 0;
+        }
+        else {
+            endTime.value = Date.now()
+            elapsed.value = endTime.value - startTime.value;
+            clearInterval(interval);
+        }
+        finished.value = true;
     };
 
     function pad(n: number, power: number = 1): string {
         const limit = Math.pow(10, power);
         let padding = '';
         let powLength = limit.toString().length;
-        for(let i = 0; i < powLength - 1; i++) {
+        for (let i = 0; i < powLength - 1; i++) {
             padding += '0';
         }
         return n < limit ? `${padding}${n}` : n.toString();
     }
 
     const formatted = computed<TimeFormat>(() => {
-        
+
         const millis = elapsed.value % 1000;
         const seconds = Math.floor((elapsed.value / 1000) % 60);
         const minutes = Math.floor((elapsed.value / 1000 / 60) % 60);
@@ -48,12 +61,9 @@ export function useTimer(updateMillis: number = 0) {
         };
     })
 
-    onMounted(() => {
-        start();
-    })
     onUnmounted(() => {
         stop();
     })
 
-    return { startTime, endTime, elapsed, formatted, start, stop };
+    return { startTime, endTime, elapsed, formatted, start, stop, hasStarted, finished };
 }
